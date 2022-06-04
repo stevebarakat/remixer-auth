14c75834c1a210478a1e217d2aa95f5d70d74750
Stephane Barakat <sb@Office-Mac.local>
2022-06-04 04:31:48 -0400

    initial

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Tailwind is for pussies!</h1>
+    </div>
+  );
+}
